0cc9761153dc217e24b775a0f8072cb24bb31df2
Talal_Bal0ch <160087743+MuhammadTa1al@users.noreply.github.com>
2024-03-31 14:40:09 +0500

    A todo list adding same things in list

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answer = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "HEY! PLEASE TYPES WHAT DO YOU WANT TO ADD: ",
+            type: "input",
+        },
+        {
+            name: "addmore",
+            message: "DO YOU REALLY WANT TO ADD AGAIN:",
+            type: "confirm",
+        },
+    ]);
+    const { todo, addmore } = answer;
+    loop = addmore;
+    if (todo) {
+        todos.push(todo);
+    }
+    else {
+        console.log("KINDLY ENTER A TODO LIST");
+    }
+}
+if (todos.length > 0) {
+    console.log("YOUR TODO LIST IS: ");
+    todos.forEach((todo) => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("NO TODO FOUND");
+}

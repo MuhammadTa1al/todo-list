0d623ffc5263699effab89afc6ca32f830959e30
Talal_Bal0ch <160087743+MuhammadTa1al@users.noreply.github.com>
2024-04-04 17:31:30 +0500

    same feature are there update

--- index.ts
+++ index.ts
@@ -1,39 +1,57 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 
-let todos: string[] = [];
-let loop = true;
+let Todos: string[] = [];
 
-while (loop) {
-  const answer: {
-    todo: string;
-    addmore: boolean;
-  } = await inquirer.prompt([
-    {
-      name: "todo",
-      message: "HEY! PLEASE TYPES WHAT DO YOU WANT TO ADD: ",
-      type: "input",
-    },
-    {
-      name: "addmore",
-      message: "DO YOU REALLY WANT TO ADD AGAIN:",
-      type: "confirm",
-    },
-  ]);
-  const { todo, addmore } = answer;
-  loop = addmore;
-  if (todo) {
-    todos.push(todo);
-  } else {
-    console.log("KINDLY ENTER A TODO LIST");
-  }
+async function createTodo(arr: string[]) {
+  do {
+    let ans = await inquirer.prompt({
+      type: "list",
+      name: "opt",
+      message: "select a option",
+      choices: ["add", "update", "view", "delete"],
+    });
+
+    if (ans.opt == "add") {
+      let add1 = await inquirer.prompt({
+        type: "input",
+        message: "add item",
+        name: "todo",
+      });
+      Todos.push(add1.todo);
+      console.log(Todos);
+    }
+    if (ans.opt == "update") {
+      let updatetodo = await inquirer.prompt({
+        type: "list",
+        message: "select item to uptade",
+        name: "todo",
+        choices: Todos.map((item) => item),
+      });
+      let AddTodo = await inquirer.prompt({
+        type: "input",
+        message: "add item",
+        name: "todo",
+      });
+      let newtodo = Todos.filter((val) => val !== updatetodo.todo);
+      Todos = [...newtodo, AddTodo.todo];
+      console.log(Todos);
+    }
+    if (ans.opt == "view") {
+      console.log(Todos);
+    }
+    if (ans.opt == "delete") {
+      let deleteTodo = await inquirer.prompt({
+        type: "list",
+        message: "select to delete the item",
+        name: "todo",
+        choices: Todos.map((item) => item),
+      });
+      let newtodo = Todos.filter((val) => val !== deleteTodo.todo);
+      Todos = [...newtodo];
+      console.log(Todos);
+    }
+  } while (true);
 }
 
-if (todos.length > 0) {
-  console.log("YOUR TODO LIST IS: ");
-  todos.forEach((todo) => {
-    console.log(todo);
-  });
-} else {
-  console.log("NO TODO FOUND");
-}
+createTodo(Todos);
